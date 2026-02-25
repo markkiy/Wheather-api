ab4869466dcc31c8b6766e3617d176e1ac03ada7
markkiy <homoki.mark@ckik.hu>
2026-02-25 20:39:34 +0100

    jsben valtoztatások/simogatások

--- scirpts/script.js
+++ scirpts/script.js
@@ -1,8 +1,13 @@
 
 const searchBtn = document.getElementById("searchBtn");
-const cityName = document.getElementById("cityName");
+const cityName = document.getElementById("location");
 const temperatureValue = document.getElementById("temperature");
 const windValue = document.getElementById("wind"); 
+const humadityValue = document.getElementById("humidity");
+const dateValue = document.getElementById("date");
+const dateDayValue = document.getElementById("dateDay");
+const iconData = document.getElementById("iconData");
+const precipitationValue = document.getElementById("precipitation");
 
 async function GetWeather() {
     const cityInput = document.getElementById("cityInput").value;
@@ -12,33 +17,77 @@ async function GetWeather() {
 
     const lat = geoData.results[0].latitude
     const lon = geoData.results[0].longitude
+    const timezone = geoData.results[0].timezone;
     
-    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto&hourly=relative_humidity_2m`)
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto&hourly=relative_humidity_2m,precipitation`)
     const weatherData = await weatherResponse.json();
 
-    console.log(getKmh(weatherData.current_weather.windspeed))
-    console.log(weatherData.current_weather)
+    console.log(weatherData.current_weather.time)
+    console.log(geoData.results[0].country)
+
+
 
     const city = geoData.results[0].name;
     const humidity = weatherData.hourly.relative_humidity_2m[0];
     const wind = getKmh(weatherData.current_weather.windspeed);
     const temp = weatherData.current_weather.temperature;
-    writeData(city, temp, humidity, wind)
+    const icon = weatherData.current_weather.weathercode
+    const perci = weatherData.hourly.precipitation[0]
+    console.log(perci)
+    writeData(city, temp, humidity, wind, timezone, icon, perci)
 
 }
 
 searchBtn.addEventListener("click", GetWeather);
 
 
-function writeData(city, temp, hum, wind) {
+function writeData(city, temp, hum, wind, timezone, icon, perci) {
     cityName.innerHTML = city;
     temperatureValue.innerHTML = `${temp}°C`;
     windValue.innerHTML = `${wind}km/h`;
+    humadityValue.innerHTML = `${hum}%`;
+    const now = new Date();
+    const options = {timeZone: `${timezone}`, year: 'numeric', month: 'short', day: 'numeric' };
+    dateDayValue.innerHTML =  now.toLocaleDateString("hu-HU", { weekday: "long",timeZone: `${timezone}` })
+    dateValue.innerHTML = now.toLocaleDateString("hu-HU",options);
+    iconData.innerHTML = getWeatherIcon(icon);
+    precipitation.innerHTML = `${perci}%`; 
     
 
 }
+
+function getWeatherIcon(code) {
+    switch (true) {
+    case (code === 0):
+      return "☀️";
+    case ([1, 2, 3].includes(code)):
+      return "🌤️";
+    case ([45, 48].includes(code)):
+      return "🌫️";
+    case ([51, 53, 55, 56, 57].includes(code)):
+      return "🌦️";
+    case ([61, 63, 65, 66, 67].includes(code)):
+      return "🌧️";
+    case ([71, 73, 75, 77].includes(code)):
+      return "❄️";
+    case ([80, 81, 82].includes(code)):
+      return "🌦️";
+    case ([85, 86].includes(code)):
+      return "🌨️";
+    case (code === 95):
+      return "⚡";
+    case ([96, 99].includes(code)):
+      return "⛈️";
+    default:
+      return "❓";
+  }
+}
+
 
 function getKmh(mph){
     const changeNumber = 1.609344;
     return Math.floor(mph * changeNumber);
 }
+
+
+
